0dfd3ace0bd0886a9211c82987fe35c7478f41a7
Shobhit-Nagpal <shobhitsnagpal@gmail.com>
2024-10-27 16:09:08 +0530

    add: pgn tokens

--- internal/token/token.go
+++ internal/token/token.go
@@ -1,1 +1,27 @@
 package token
+
+type TokenType string
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+const (
+	PERIOD  = "."
+	ASTERIX = "*"
+	STRING  = "STRING"
+	INTEGER = "INTEGER"
+
+	LBRACKET = "["
+	RBRACKET = "]"
+
+	LPAREN = "("
+	RPAREN = ")"
+
+	LANGLE = "<"
+	RANGLE = ">"
+
+	NAG    = "NAG"
+	SYMBOL = "SYMBOL"
+)
